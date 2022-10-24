67805f7facb3367429de6379b046fbd38f650deb
Ivan Franco <ivan.franco@derivco.co.uk>
2022-10-24 13:33:39 +0200

    adding Wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,8 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
+import Wrapper from "../Helpers/Wrapper";
+
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -48,7 +50,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -75,7 +77,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
